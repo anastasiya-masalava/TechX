323dd80234134eafe5faf34f11b37bba67404a2c
zhixianliding <91989770+zhixianliding@users.noreply.github.com>
2021-10-18 17:18:40 -0400

    Histogram for funding_total_usd

---
+++ exploratory_3.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('clean_companies.csv')
+
+minidf = df[['name', 'funding_total_usd']]
+minidf = minidf[minidf['funding_total_usd'] < 5000000000]
+minidf = minidf.dropna(axis=0, how='any')
+
+print(minidf)
+plt.hist(minidf["funding_total_usd"], bins=20)
+plt.yscale('log')
+plt.title('Distribution of Funding (Less than $5 Billion) Per Company (n>60,000)')
+plt.xlabel('Funding (USD), Bin Size: $250 Million')
+plt.ylabel('Number of Companies')
+plt.show()
+
