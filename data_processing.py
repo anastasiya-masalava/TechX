6b5c07d9142d3263dc0b820315be5e49924d157a
anastasiamasalova2020 <masnas2004@gmail.com>
2021-11-21 11:59:38 -0500

    Milestone 3

--- data_processing.py
+++ data_processing.py
@@ -4,7 +4,10 @@ df = pd.read_csv("clean_companies.csv")
 df = df.drop(['permalink', 'state_code', 'region', 'first_funding_at','last_funding_at'], axis = 1)
 
 df['funding_total_usd'].fillna(df['funding_total_usd'].mean(), inplace=True)
-
+df['homepage_url'].fillna("Not found", inplace=True)
+df['country_code'].fillna("Not found", inplace=True)
+df['city'].fillna("Not found", inplace=True)
+df['founded_at'].fillna("Not found", inplace=True)
 df.to_csv("new_companies.csv", index = False)
 
 '''
