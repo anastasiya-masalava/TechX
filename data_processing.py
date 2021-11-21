a0ef84028ac6059b7a3189da2cc41347a5ea4796
zhixianliding <91989770+zhixianliding@users.noreply.github.com>
2021-11-21 11:48:49 -0500

    Deleted useless columns.

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv("companies.csv")
+df = pd.read_csv("clean_companies.csv")
 df = df.drop(['permalink', 'state_code', 'region', 'first_funding_at','last_funding_at'], axis = 1)
 
-#mini_df['Age'].fillna(mini_df['Age'].mean(), inplace=True)
+df['funding_total_usd'].fillna(df['funding_total_usd'].mean(), inplace=True)
 
+df.to_csv("new_companies.csv", index = False)
 
+'''
 string_data=str(df)
 list_data=string_data.split(",")
 print(list_data)
+'''
