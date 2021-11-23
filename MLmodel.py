7e2d61bda3013ac258ce80537db75f85caa5b3b8
anastasiamasalova2020 <masnas2004@gmail.com>
2021-11-22 19:25:15 -0500

    Milestone 4

--- MLmodel.py
+++ MLmodel.py
@@ -5,15 +5,15 @@ import seaborn as sns
 df = pd.read_csv("datasets/clean_companies.csv")
 df = df[["category_list", "funding_total_usd"]]
 df.rename(columns={"category_list":"category", "funding_total_usd":"funding"}, inplace=True)
-df = df.dropna(axis=0, how = 'any')
+#df = df.dropna(axis=0, how = 'any')
 
 
 newdf=pd.DataFrame(columns=["category", "funding"])
 index=0
 
 for category in df["category"]:
-    catList=category.split("|")
     try:
+        catList=category.split("|")
         if len(catList) == 1:
             fund = df["funding"][index]
             newdf = newdf.append({'category':category, "funding":fund}, ignore_index=True)
@@ -25,4 +25,5 @@ for category in df["category"]:
     except:
         pass
     index += 1
+newdf = newdf.dropna(axis=0, how = 'any')
 newdf.to_csv("datasets/new_df.csv", index = False)
